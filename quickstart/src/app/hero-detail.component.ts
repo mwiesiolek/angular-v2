2704726a1841bab950e6aeb45536f3599867d139
m.wiesiolek <m.wiesiolek@globallogic.com>
2017-06-18 20:34:25 +0200

    styles up components in separate files

--- quickstart/src/app/hero-detail.component.ts
+++ quickstart/src/app/hero-detail.component.ts
@@ -1,15 +1,14 @@
 import {Component, Input, OnInit} from "@angular/core";
-import {ActivatedRoute, Params} from '@angular/router';
-import {Location} from '@angular/common';
-
-import {Hero} from './hero';
-import {HeroService} from './hero.service';
-
-import 'rxjs/add/operator/switchMap';
+import {ActivatedRoute, Params} from "@angular/router";
+import {Location} from "@angular/common";
+import {Hero} from "./hero";
+import {HeroService} from "./hero.service";
+import "rxjs/add/operator/switchMap";
 
 @Component({
   selector: 'hero-detail',
-  templateUrl: './hero-detail.component.html'
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
 })
 
 export class HeroDetailComponent implements OnInit {
@@ -22,9 +21,10 @@ export class HeroDetailComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.params
-        .switchMap((params: Params) => this.heroService.getHero(+params['id']))
-        .subscribe(hero => this.hero = hero);
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
   }
+
   goBack(): void {
     this.location.back();
   }
