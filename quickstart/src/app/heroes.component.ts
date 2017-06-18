60a98453d52da196f5f5c80a779bd1d4ca857359
m.wiesiolek <m.wiesiolek@globallogic.com>
2017-06-18 20:24:55 +0200

    styles up components in separate files

--- quickstart/src/app/heroes.component.ts
+++ quickstart/src/app/heroes.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from "@angular/core";
+import {Router} from "@angular/router";
+
 import {Hero} from "./hero";
 import {HeroService} from "./hero.service";
 
@@ -12,7 +14,8 @@ export class HeroesComponent implements OnInit {
   selectedHero: Hero;
   heroes: Hero[];
 
-  constructor(private heroService: HeroService) {
+  constructor(private heroService: HeroService,
+              private router: Router) {
   }
 
   ngOnInit(): void {
@@ -25,5 +28,9 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
   }
 }
