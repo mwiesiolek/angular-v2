21851caa9816cd4d0489570727d248249cc4520a
m.wiesiolek <m.wiesiolek@globallogic.com>
2017-06-18 21:55:15 +0200

    adds js web server

--- quickstart/src/app/app.module.ts
+++ quickstart/src/app/app.module.ts
@@ -2,26 +2,34 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
 // demo components, service, etc
 import {AppComponent} from "./app.component";
 import {HeroesComponent} from "./heroes.component";
 import {HeroDetailComponent} from "./hero-detail.component";
 import {DashboardComponent} from "./dashboard.component";
 import {HeroService} from "./hero.service";
+import {HeroSearchComponent} from "./hearo-search.component";
 // routing
 import {AppRoutingModule} from "./app-routing.module";
+// in memory web api
+import {InMemoryWebApiModule} from "angular-in-memory-web-api";
+import {InMemoryDataService} from "./in-memory-data.service";
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
   declarations: [
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   bootstrap: [
     AppComponent
