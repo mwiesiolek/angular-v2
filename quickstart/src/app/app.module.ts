30fcc0435f62567719cc804e3c5c84586a600523
m.wiesiolek <m.wiesiolek@globallogic.com>
2017-06-18 16:54:36 +0200

    exports logic to different components

--- quickstart/src/app/app.module.ts
+++ quickstart/src/app/app.module.ts
@@ -2,37 +2,20 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {FormsModule} from "@angular/forms";
-import {RouterModule} from "@angular/router";
 // demo components, service, etc
 import {AppComponent} from "./app.component";
 import {HeroesComponent} from "./heroes.component";
 import {HeroDetailComponent} from "./hero-detail.component";
 import {DashboardComponent} from "./dashboard.component";
 import {HeroService} from "./hero.service";
+// routing
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'detail/:id',
-        component: HeroDetailComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      }
-    ])
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
